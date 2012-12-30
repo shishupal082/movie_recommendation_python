171d865b0c9f024613f480c0e5b9013f61fe7ab2
Shishupal Kumar <shishupal.kumar@hashedin.com>
2012-12-31 04:15:32 +0530

    Most Watched Genre Completed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     
     #movies_dict = hardcoded_data.hardcoded_movies_data()
     print("_____________________________________________________________")
-    #******************Most Watched Movie **************************************
+    #****************** Most Watched Movie **************************************
     most_watched_movie_count = 0
     most_watched_movie_id = 0
     for movie_id in movies_dict :
@@ -18,9 +18,9 @@ def main():
     
     print("Most Watched Movie:\t\t\t" + movies_dict[most_watched_movie_id]['movie_title'])
     
-    #Top Movie By Genre **************************************
+    #****************** Top Movie By Genre **************************************
     
-    input_genre = "war"
+    input_genre = "thriller"
     movie_id_list = []
     for movie_id in movies_dict :
         genre_list = movies_dict[movie_id]["genres"]
@@ -35,6 +35,27 @@ def main():
         if movie_rating > max_rating :
             max_rating_movie_id_in_this_genre = movie_id
     print("Top movie by genre'("+input_genre+")':\t"+movies_dict[max_rating_movie_id_in_this_genre]['movie_title'])
+    
+    #**************** Most Watched Genre ****************************************
+    genre_count_dictionary = {"unknown":0,"action":0,"adventure":0,"animation":0,"children's":0,"comedy":0,"crime":0,"documentary":0,"drama":0,"fantasy":0,"film-noir":0,"horror":0,"musical":0,"mystery":0,"romance":0,"sci-fi":0,"thriller":0,"war":0,"western":0}
+    
+    
+    
+    for movie_id in movies_dict :
+        genre_list = movies_dict[movie_id]["genres"]
+        for genre in genre_list :
+            genre_count_dictionary[genre] = 1 + genre_count_dictionary[genre]
+    
+    #print(genre_count_dictionary)
+            
+    max_genre_count = genre_count_dictionary["unknown"]
+    genre = "unknown"
+    for key_genre in genre_count_dictionary :
+        if genre_count_dictionary[key_genre] > max_genre_count :
+            max_genre_count = genre_count_dictionary[key_genre]
+            genre = key_genre
+    
+    print("Most Watched Genre:\t\t"+genre)
     print("_____________________________________________________________")
 if __name__ == "__main__":
     main()
